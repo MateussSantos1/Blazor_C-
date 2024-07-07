111e20105f0c293df6bd224a3da79874f8ed5fb2
Matt <mateuservico@gmail.com>
2024-07-07 16:07:47 -0300

    Crud_Simples_Com_Blazor_Web_app

---
+++ Crud_Blazor_EF/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Crud_Blazor_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Blazor_EF.Data
+{
+
+
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; } = null!;
+    }
+}
---
+++ Crud_Blazor_EF/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud_Blazor_EF.Models
+{
+    public class Categoria
+    {
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+    }
+}
